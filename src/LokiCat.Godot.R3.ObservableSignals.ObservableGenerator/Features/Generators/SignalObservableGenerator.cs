74544d75847899be0fd3956de91910624137e3f2
Alex Hall <alex@alexhall.org>
2025-04-19 16:27:27 -0500

    Fix namespace

--- src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/Generators/SignalObservableGenerator.cs
+++ src/LokiCat.Godot.R3.ObservableSignals.ObservableGenerator/Features/Generators/SignalObservableGenerator.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using System.Linq;
+﻿using System.Linq;
 using LokiCat.GodotNodeInterfaces.Observables.ObservableGenerator.Features.SyntaxHelpers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-namespace LokiCat.GodotNodeInterfaces.Observables.ObservableGenerator.Features.Generators;
+namespace LokiCat.Godot.R3.ObservableSignals.ObservableGenerator.Features.Generators;
 
 [Generator]
 public sealed class SignalObservableGenerator : ISourceGenerator
